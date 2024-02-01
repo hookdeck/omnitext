c96302e346794a9faea9ec53802c08b6d6385dbe
Phil Leggetter <phil@leggetter.co.uk>
2024-02-01 00:23:03 +0000

    feat: email threading

--- transformations/vonage-sms-to-postmark-email.mjs
+++ transformations/vonage-sms-to-postmark-email.mjs
@@ -1,4 +1,8 @@
-const conversationId = "a-unique-conversation-id";
+// See info on threading emails:
+// - https://postmarkapp.com/support/article/1276-threading-unthreading-messages
+// - https://www.ombulabs.com/blog/rails/emails/tutorial/send-emails-that-thread-with-rails.html
+// TODO: dynamically generate the domain
+const conversationId = "<omnitext/conversation/1@leggetter.co.uk>";
 const replayToEmail = process.env.REPLY_TO_EMAIL;
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
@@ -17,15 +21,16 @@ const smsToEmail = (request, context) => {
         Name: "Message-ID",
         Value: conversationId,
       },
-      {
-        Name: "In-Reply-To",
-        Value: conversationId,
-      },
-      // References should be a comma separated list of previous messages
-      {
-        Name: "References",
-        Value: conversationId,
-      },
+      // Removing these appears to resolve the out of order email threading problem
+      // {
+      //   Name: "In-Reply-To",
+      //   Value: conversationId,
+      // },
+      // // References should be a comma separated list of previous messages
+      // {
+      //   Name: "References",
+      //   Value: conversationId,
+      // },
     ],
   };
 
